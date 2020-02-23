338962cfbb424b6b31ba468666c68db9812b2529
Patrick Wiener <wiener@fzi.de>
2020-02-23 17:11:55 +0100

    differentiate between jre and debian base img

--- docker-build.sh
+++ docker-build.sh
@@ -4,6 +4,9 @@ DOCKERHUB_REPO=$1
 BASE_IMG_JRE_DEFAULT=$2
 BASE_IMG_JRE_ARM32V7=$3
 BASE_IMG_JRE_ARM64V8=$4
+BASE_IMG_DEBIAN_DEFAULT=$5
+BASE_IMG_DEBIAN_ARM32V7=$6
+BASE_IMG_DEBIAN_ARM64V8=$7
 
 ACTIVEMQ_VERSION=5.15.9
 CONSUL_VERSION=1.7.0
@@ -19,17 +22,28 @@ cp_qemu() {
 }
 
 docker_build_push() {
-	SERVICE=$1
-	VERSION=$2
+	TYPE=$1
+	SERVICE=$2
+	VERSION=$3
 	IMG_NAME_DEFAULT=$DOCKERHUB_REPO/$SERVICE:$VERSION
 	IMG_NAME_AMD64=$DOCKERHUB_REPO/$SERVICE:amd64-$VERSION
 	IMG_NAME_ARM32V7=$DOCKERHUB_REPO/$SERVICE:arm32v7-$VERSION
 	IMG_NAME_ARM64V8=$DOCKERHUB_REPO/$SERVICE:arm64v8-$VERSION
 
+	if [ "$TYPE" == "jre" ]; then
+			BASE_IMG_DEFAULT = $BASE_IMG_JRE_DEFAULT
+			BASE_IMG_ARM32V7 = $BASE_IMG_JRE_ARM32V7
+			BASE_IMG_ARM64V8 = $BASE_IMG_JRE_ARM64V8
+	elif [ "$TYPE" == "debian" ]; then
+			BASE_IMG_DEFAULT = $BASE_IMG_DEBIAN_DEFAULT
+			BASE_IMG_ARM32V7 = $BASE_IMG_DEBIAN_ARM32V7
+			BASE_IMG_ARM64V8 = $BASE_IMG_DEBIAN_ARM64V8
+	fi
+
 	# build docker images
 	echo "======> Building Docker Image: [ $IMG_NAME_DEFAULT, $IMG_NAME_AMD64 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_JRE_DEFAULT \
+	--build-arg BASE_IMG=$BASE_IMG_DEFAULT \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
 	-t $IMG_NAME_DEFAULT -t $IMG_NAME_AMD64 \
@@ -37,7 +51,7 @@ docker_build_push() {
 
 	echo "======> Building Docker Image: [ $IMG_NAME_ARM32V7 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_JRE_ARM32V7 \
+	--build-arg BASE_IMG=$BASE_IMG_ARM32V7 \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
 	-t $IMG_NAME_ARM32V7 \
@@ -45,7 +59,7 @@ docker_build_push() {
 
 	echo "======> Building Docker Image: [ $IMG_NAME_ARM64V8 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_JRE_ARM64V8 \
+	--build-arg BASE_IMG=$BASE_IMG_ARM64V8 \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
 	-t $IMG_NAME_ARM64V8 \
@@ -67,7 +81,7 @@ docker_build_push() {
 docker_build_push_multiarch_existing() {
 	SERVICE=$1
 	VERSION=$2
-	BASE_IMG_SERVICE=$SERVICE:$VERSION
+	BASE_IMG=$SERVICE:$VERSION
 	IMG_NAME_DEFAULT=$DOCKERHUB_REPO/$SERVICE:$VERSION
 	IMG_NAME_AMD64=$DOCKERHUB_REPO/$SERVICE:amd64-$VERSION
 	IMG_NAME_ARM32V7=$DOCKERHUB_REPO/$SERVICE:arm32v7-$VERSION
@@ -76,7 +90,7 @@ docker_build_push_multiarch_existing() {
 	# build docker images
 	echo "======> Building Docker Image: [ $IMG_NAME_DEFAULT, $IMG_NAME_AMD64 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_SERVICE \
+	--build-arg BASE_IMG=$BASE_IMG \
 	--build-arg ARCH=amd64 \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
@@ -85,7 +99,7 @@ docker_build_push_multiarch_existing() {
 
 	echo "======> Building Docker Image: [ $IMG_NAME_ARM32V7 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_SERVICE \
+	--build-arg BASE_IMG=$BASE_IMG \
 	--build-arg ARCH=arm \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
@@ -94,7 +108,7 @@ docker_build_push_multiarch_existing() {
 
 	echo "======> Building Docker Image: [ $IMG_NAME_ARM64V8 ]"
 	docker build --pull \
-	--build-arg BASE_IMG=$BASE_IMG_SERVICE \
+	--build-arg BASE_IMG=$BASE_IMG \
 	--build-arg ARCH=arm64 \
 	--build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	--build-arg VCS_REF=`git rev-parse --short HEAD` \
@@ -120,7 +134,7 @@ do
 
   if [ "$i" == "activemq" ]; then
 		cp_qemu $i
-		docker_build_push $i $ACTIVEMQ_VERSION
+		docker_build_push "jre" $i $ACTIVEMQ_VERSION
 
   elif [ "$i" == "consul" ]; then
 		cp_qemu $i
@@ -128,7 +142,7 @@ do
 
   elif [ "$i" == "couchdb" ]; then
 		cp_qemu $i
-		docker_build_push $i $COUCHDB_VERSION
+		docker_build_push "debian" $i $COUCHDB_VERSION
 
   elif [ "$i" == "influxdb" ]; then
 		cp_qemu $i
@@ -136,11 +150,11 @@ do
 
   elif [ "$i" == "kafka" ]; then
 		cp_qemu $i
-		docker_build_push $i $KAFKA_VERSION
+		docker_build_push "jre" $i $KAFKA_VERSION
 
   elif [ "$i" == "zookeeper" ]; then
 		cp_qemu $i
-		docker_build_push $i $ZK_VERSION
+		docker_build_push "jre" $i $ZK_VERSION
   fi
 
 done
